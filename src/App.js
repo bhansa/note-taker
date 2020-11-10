887cca43332a7ba9293689da4cf8ed42187f380c
Travis CI <travis@travis-ci.org>
2020-11-10 15:29:59 +0530

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter/Filter";
 import ButtonFloat from "./ButtonFloat/ButtonFloat";
-import "./App.scss";
-import dummy_card_data from "./dummy_data";
 import Card from "./Card/Card";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: dummy_card_data,
-      filteredResult: dummy_card_data,
-      resultCount: 0,
-      isResultVisible: false,
-    };
-  }
-  handleInputChange = (event) => {
-    let inputValue = event.target.value;
-    // creating regex
-    let regex = new RegExp(inputValue, "i");
-    let filteredResult = this.state.data.filter(
-      (val) => regex.test(val.title) === true || regex.test(val.desc)
-    );
-    this.setState({
-      filteredResult: filteredResult,
-      resultCount: filteredResult.length,
-      isResultVisible: () => {
-        return inputValue.length > 0;
-      },
+import dummy_card_data from "./dummy_data";
+import "./App.scss";
+
+import { db } from "./firebase";
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    // data: dummy_card_data,
+    // filteredResult: dummy_card_data,
+    // resultCount: 0,
+    // isResultVisible: false,
+  ]);
+  // function handleInputChange(event){
+  //   let inputValue = event.target.value;
+  //   // creating regex
+  //   let regex = new RegExp(inputValue, "i");
+  //   let state.filteredResult = this.state.data.filter(
+  //     (val) => regex.test(val.title) === true || regex.test(val.desc)
+  //   );
+  //   // this.setState({
+  //   //   filteredResult: filteredResult,
+  //   //   resultCount: filteredResult.length,
+  //   //   isResultVisible: () => {
+  //   //     return inputValue.length > 0;
+  //   //   },
+  //   // });
+  //   console.log(this.state.filteredResult.length);
+  // };
+  useEffect(() => {
+    db.collection("notes").onSnapshot((snapshot) => {
+      setNotes(snapshot.docs.map((doc) => doc.data()));
     });
-    console.log(this.state.filteredResult.length);
-  };
-  render() {
-    const { resultCount, isResultVisible, filteredResult } = this.state;
-    return (
-      <div className="App">
-        <h1>Note Taker</h1>
-        <ButtonFloat />
-        <Filter
-          handleInputChange={this.handleInputChange}
-          resultCount={resultCount}
-          isResultVisible={isResultVisible}
-          data={this.state.data}
-        />
-        <Card data={filteredResult} />
+  }, []);
+  return (
+    <div className="App">
+      <h1>Note Taker</h1>
+      <ButtonFloat />
+      {/* <Filter
+        handleInputChange={this.handleInputChange}
+        resultCount={state.resultCount}
+        isResultVisible={state.isResultVisible}
+        data={this.state.data}
+      /> */}
+      <div className="Card-container">
+        {notes.map((note, index) => {
+          return <Card data={note} key={index} />;
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,21 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Card.scss";
 
-class Card extends Component {
-  render() {
-    return (
-      <div className="Card-container">
-        {this.props.data.map((data, index) => (
-          <a href="/note/{index}" key={index}>
-            <div className="Card">
-              <h2>{data.title}</h2>
-              <p>{data.desc}</p>
-            </div>
-          </a>
-        ))}
+const Card = ({ data, index }) => {
+  return (
+    <a href="/note/{index}">
+      <div className="Card">
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
       </div>
-    );
-  }
-}
+    </a>
+  );
+};
 
 export default Card;
